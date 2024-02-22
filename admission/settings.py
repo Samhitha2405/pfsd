4dcff820470f10a89dfb3ee1ec7d72dcf6087f55
Vishnu Samhitha <2200031106@kluniversity.in>
2024-02-22 21:50:31 +0530

    update

--- admission/settings.py
+++ admission/settings.py
@@ -73,13 +73,14 @@ WSGI_APPLICATION = 'admission.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 '''
 DATABASES = {
     'default':{
@@ -91,6 +92,7 @@ DATABASES = {
         'PORT':'5432'
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
